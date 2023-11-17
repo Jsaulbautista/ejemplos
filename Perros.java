92b8f3515eb085da4b9373fb3603aa91a869dafa
Jsaulbautista <150806077+Jsaulbautista@users.noreply.github.com>
2023-11-17 12:10:54 -0500

    Add files via upload

---
+++ Perros.java
@@ -0,0 +1,20 @@
+public class Perros {
+    // atributos
+    string raza;
+    string nombre;
+    int edad;
+    string color;
+    // constructor
+    Perros(string nuevonmbre){
+        nombre = nuevonmbre;
+    }
+    int getEdad(){
+        return  edad;
+    }
+    string getNombre(){
+        return Nombre;
+    }
+    
+
+}
+
